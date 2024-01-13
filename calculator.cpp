1d1ed722e2fcc2637ab5acd3dd89c212673b4619
Saram Ali <saramali15792@gmail.com>
2024-01-13 21:04:16 +0500

    update function calculator

--- calculator.cpp
+++ calculator.cpp
@@ -6,12 +6,8 @@ int sum(int a,int b){
 int mul(int a,int b){
     return a*b;
 }
-int sub(int a,int b){
-    return a-b;
-}
-int rem(int a,int b){
-    return a/b;
-}
+
+
 int main(){
 int a,b;
 cout<<"*******welcome to calculator program*****"<<endl;
@@ -19,8 +15,7 @@ cout<<"enter the number u wnat"<<endl;
 cin>>a>>b;
 cout<<sum(a,b)<<endl;
 cout<<mul(a,b)<<endl;
-cout<<rem(a,b)<<endl;
-cout<<sub(a,b)<<endl;
+
 
     return 0;
 }
